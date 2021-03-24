1046086841135ebddcefba49691c518a41f00ea2
JOSEPH-8546-cyber <manueljosephtrabajo@gmail.com>
2021-03-24 18:58:19 +0000

    se hace la api con sus get y los toquen y registro de cada usuario

--- src/main.py
+++ src/main.py
@@ -2,13 +2,16 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, json
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, JWTManager
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets_Data, Chart_Data, Starships_Data, FavList_Planets, FavList_Charts,  FavList_Starships
+import datetime
 #from models import Person
 
 app = Flask(__name__)
@@ -19,6 +22,7 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -30,16 +34,244 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+
+#users FUNCTIONS
+
+#get all users
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    all_users = User.query.all()
+    all_users_serialized = list(map(lambda user: user.serialize(), all_users))
+    return jsonify(all_users_serialized),200
+
+#get only user
+@app.route('/user/<int:id>', methods=['GET'])
+def get_only_user(id):
+    only_user = User.query.filter_by(id=id).first()
+    if not only_user:
+        return jsonify({"msg": "user not found"}),404
+    only_user = only_user.serialize()
+    return jsonify(only_user),200
+
+#######################################
+#Register
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        username = request.json.get("username", None)
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
+        bio = request.json.get("bio", None)
+
+    if not username:
+        return jsonify({"msg": "The username is required" }), 404
+    if not email:
+        return jsonify({"msg": "The email is required"}), 404
+    if not password:
+        return jsonify({'msg': "The password is required"}), 404
+    if not bio:
+        return jsonify({'msg': "The bio is required"}), 404
+    
+    new_user = User(username=username, email=email, password=generate_password_hash(password), bio=bio)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify("All good, new user add")
+
+
+
+#################################
+#Login
+@app.route('/starSession', methods=['POST'])
+def starSession():
+    if request.method == 'POST':
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
+
+    if not email:
+        return jsonify({'msg': "The email is required for login"}), 404
+    if not password:
+        return jsonify({'msg': "The password is required for login"}), 404
+
+    Find_user = User.query.filter_by(email=email).first()
+
+    if not Find_user:
+        return jsonify({'msg': "The email or password is incorrect"}),404
+    if not check_password_hash(Find_user.password, password):
+        return jsonify({'msg': "The email or password is incorrect"}),404
+
+    expires=datetime.timedelta(days=2)
+    access_token = create_access_token(indentity=Find_user.email, expires_delta=expires)
+
+    data = {
+        "token": access_token,
+        "successful": True
     }
 
-    return jsonify(response_body), 200
+    return jsonify(data), 200
+
+
+    
+
+
+    
+#FUNCTIONS de los favoritos
+
+#list de favoritos de todos planetas
+@app.route('/favorites/planets', methods=['GET'])
+def get_all_favorites_planets():
+    all_favorites_planets =  FavList_Planets.query.all()
+    all_favorites_planets = list(map(lambda all_favorites: all_favorites.serialize(), all_favorites_planets)),
+    return jsonify(all_favorites_planets), 200
+
+#list de cada planeta favorito
+@app.route('/favorite/planet/<int:id>', methods=['GET'])
+def get_only_favorite(id):
+    only_favorite_planet = FavList_Planets.query.filter_by(id=id).first()
+    if not only_favorite_planet:
+        return jsonify({"msg": "favorite not found"}),404
+    only_favorite_planet = only_favorite_planet.serialize()
+    return jsonify(only_favorite_planet)
+
+
+#list de todos los charts favoritos
+@app.route('/favorites/charts', methods=['GET'])
+def get_all_favorites_charts():
+    all_favorites_charts = FavList_Charts.query.all()
+    all_favorites_charts = list(map(lambda all_charts: all_charts.serialize(), all_favorites_charts))
+    return jsonify(all_favorites_charts)
+
+
+#list de cada chart favorito
+@app.route('/favorite/chart/<int:id>', methods=['GET'])
+def get_only_favorite_chart(id):
+    only_favorite_chart = FavList_Charts.query.filter_by(id=id).first()
+    if not only_favorite_chart:
+        return jsonify({"msg": "favorite chart not found"}),404
+    only_favorite_chart = only_favorite_chart.serialize()
+    return jsonify(only_favorite_chart)
+
+
+#list de todos los starships favoritos
+@app.route('/favorites/starships', methods=['GET'])
+def get_all_favorites_starships():
+    all_favorites_starships = FavList_Starships.query.all()
+    all_favorites_starships = list(map(lambda favorites_starships: favorites_starships.serialize(), all_favorites_starships))
+    return jsonify(all_favorites_starships)
+
+
+#list de cada starship favorito
+@app.route('/favorite/starship/<int:id>', methods=['GET'])
+def get_only_favorite_starship(id):
+    only_favorite_starship = FavList_Starships.query.filter_by(id=id).first()
+    if not only_favorite_starship:
+        return jsonify({"msg": "starship favorite not found"}),404
+    only_favorite_starship = only_favorite_starship.serialize()
+    return jsonify(only_favorite_starship)
+
+    
+#FUNCTIONS PLANETS
+
+#get all information planets
+@app.route('/planets', methods=['GET'])
+def show_all_planets():
+    all_planets = Planets_Data.query.all()
+    all_planets_serialized = list(map(lambda planets: planets.serialize(), all_planets))
+    return jsonify(all_planets_serialized),200
+
+
+    
+#get only planet
+@app.route('/planet/<int:id>', methods=['GET']) 
+def get_only_planet(id):
+    only_planet = Planets_Data.query.filter_by(id=id).first()
+    if not only_planet:
+        return jsonify({"msg":"Planet not found"}),404
+    only_planet = only_planet.serialize()
+    return jsonify(only_planet),200
+
+
+
+#get all data planet
+@app.route('/planet/data', methods=['GET'])
+def get_planet_data():
+    planet_data = Planets_Data.query.all()
+    planet_data_serialized = list(map(lambda data: data.serialize_data(), planet_data))
+    return jsonify(planet_data_serialized),200
+
+
+
+
+
+#FUNCTIONS CHARTS
+
+#get all info charts
+@app.route('/charts', methods=['GET'])
+def show_all_charts():
+    all_charts = Chart_Data.query.all()
+    all_charts_serialized = list(map(lambda charts: charts.serialize(), all_charts))
+    return jsonify(all_charts_serialized), 200
+
+
+
+ #get only chart
+@app.route('/chart/<int:id>', methods=['GET'])
+def get_only_chart(id):
+    only_chart = Chart_Data.query.filter_by(id=id).first()
+    if not only_chart:
+        return jsonify({"msg": "chart not found"}),404
+    only_chart = only_chart.serialize()
+    return jsonify(only_chart),200
+
+
+
+#get all data chart
+@app.route('/chart/data', methods=['GET'])
+def get_chart_data():
+    chart_data = Chart_Data.query.all()
+    char_data_serialized = list(map(lambda data: data.serialize_data(), chart_data))
+    return jsonify(char_data_serialized),200
+
+
+
+
+#FUNCTIONS STARSHIPS 
+
+#get all info starships   
+@app.route('/starships', methods=['GET'])
+def show_all_starships():
+    all_starships = Starships_Data.query.all()
+    all_starships_serialized = list(map(lambda starships: starships.serialize(), all_starships))
+    return jsonify(all_starships_serialized),200
+
+
+
+#get only starship
+@app.route('/starship/<int:id>', methods=['GET'])
+def get_only_starship(id):
+    only_starship = Starships_Data.query.filter_by(id=id).first()
+    if not only_starship:
+        return jsonify({"msg": "starship not found"}),404
+    only_starship = only_starship.serialize()
+    return jsonify(only_starship),200
+
+
+
+#get all data starship
+@app.route('/starship/data', methods=['GET'])
+def get_starship_data():
+    starship_data = Starships_Data.query.all()
+    starship_data_serialized = list(map(lambda data: data.serialize_data(), starship_data))
+    return jsonify(starship_data_serialized),200
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
--- src/models.py
+++ src/models.py
@@ -2,18 +2,276 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+#data usuarios
 class User(db.Model):
+    __tablename__ = "username_data"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=True)
+    bio = db.Column(db.String(200), nullable=False)
+    fav_planets = db.relationship("FavList_Planets", backref="user", lazy=True)
+    fav_charts = db.relationship("FavList_Charts", backref="user", lazy=True)
+    fav_starships = db.relationship("FavList_Starships", backref="user", lazy=True)
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f"<username_data {self.username} -  {self.id} - {self.is_active}>"
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
+            "bio": self.bio,
+            "fav_planets": list(map(lambda fav_planet: fav_planet.serialize2(), self.fav_planets)),
+            "fav_charts": list(map(lambda fav_chart: fav_chart.serialize2(), self.fav_charts)),
+            "fav_starships": list(map(lambda fav_starships: fav_starships.serialize2(), self.fav_starships))
             # do not serialize the password, its a security breach
+         }
+
+
+#lista de favoritos
+class FavList_Planets(db.Model):
+    __tablename__ = 'favlist_planets'
+    id = db.Column(db.Integer, primary_key=True)
+    id_planets = db.Column(db.Integer,db.ForeignKey("planets_data.id"), nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("username_data.id"), nullable=False)
+    comments = db.Column(db.String(200), nullable=True)
+
+    def __repr__(self):
+        return f"<favlist_planets {self.user_id}>" 
+    
+
+    def serialize(self):
+        return {
+        "id": self.id,
+        "id_planet": self.id_planets,
+        "name_planet": self.planets_data.name_planet,
+        "user_id": self.user_id,
+        "user_name": self.user.username,
+        "comments": self.comments
         }
+    
+    def serialize2(self):
+        return {
+            "id": self.planets_data.id,
+            "name_planet": self.planets_data.name_planet,
+            "diameter": self.planets_data.diameter,
+            "rotation_period": self.planets_data.rotation_period,
+            "orbital_period": self.planets_data.orbital_period,
+            "gravity": self.planets_data.gravity,
+            "population": self.planets_data.population,
+            "climate": self.planets_data.climate,
+            "terrain": self.planets_data.terrain,
+            "surface_water": self.planets_data.surface_water
+        }
+
+#Datos de los planetas
+class Planets_Data(db.Model):
+    __tablename__ = 'planets_data'
+    id = db.Column(db.Integer, primary_key=True)
+    name_planet = db.Column(db.String(100), nullable=False) 
+    diameter = db.Column(db.String(100), nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.String(100), nullable=False)
+    gravity = db.Column(db.String(100), nullable=False)
+    gravity_type = db.Column(db.String(100), nullable=False)
+    population = db.Column(db.String(100), nullable=False)
+    climate = db.Column(db.String(100), nullable=False)
+    terrain = db.Column(db.String(100), nullable=False)
+    surface_water = db.Column(db.String(15), nullable=False)
+    fav_planets_favlist = db.relationship("FavList_Planets", backref="planets_data", lazy=True)
+
+    def __repr__(self):
+        return f"<planets_data {self.name_planet} - {self.id}>"
+
+    def serialize(self):
+         return {
+             "name_planet": self.name_planet,
+             "id": self.id
+         }
+
+    def serialize_data(self):
+        return {
+            "id": self.id,
+            "name_planet": self.name_planet,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "gravity": self.gravity_type,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water  
+        }
+
+
+ #lista de charts favoritos
+class FavList_Charts(db.Model):
+     __tablename__ = 'favlist_chart'
+     id = db.Column(db.Integer, primary_key=True)
+     id_chart = db.Column(db.Integer, db.ForeignKey("chart_data.id"), nullable=False)
+     user_id = db.Column(db.Integer, db.ForeignKey("username_data.id"))
+     comments = db.Column(db.String(500), nullable=False)
+
+     def __repr__(self):
+         return f"<favlist_chart {self.user_id} - {self.id_chart}>"
+
+     def serialize(self):
+            return {
+                "id": self.id,
+                "user_id": self.user_id,
+                "name_chart": self.chart_data.name_chart,
+                "id_chart": self.id_chart,
+                "user_name": self.user.username,
+                "comments": self.comments
+     }
+
+     def serialize2(self):
+         return {
+             "id": self.chart_data.id,
+             "name_chart": self.chart_data.name_chart,
+             "height": self.chart_data.height,
+             "mass": self.chart_data.mass,
+             "hair_color": self.chart_data.hair_color,
+             "skin_color": self.chart_data.skin_color,
+             "eye_color": self.chart_data.eye_color,
+             "birth_year": self.chart_data.birth_year,
+             "gender": self.chart_data.gender
+         }
+
+
+#data de los charts
+class Chart_Data(db.Model):
+    __tablename__ = 'chart_data'
+    id = db.Column(db.Integer, primary_key=True)
+    name_chart = db.Column(db.String(100), nullable=False)
+    height = db.Column(db.String(100), nullable=False)
+    mass = db.Column(db.String(100), nullable=False)
+    hair_color = db.Column(db.String(90), nullable=False)
+    skin_color = db.Column(db.String(100), nullable=False)
+    eye_color = db.Column(db.String(100), nullable=False)
+    birth_year  = db.Column(db.String(100), nullable=False)
+    gender  = db.Column(db.String(100), nullable=False)
+    fav_favlist_chart = db.relationship("FavList_Charts", backref="chart_data")
+
+    def __repr__(self):
+        return f"<chart_data {self.name_chart} - {self.id}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name_chart": self.name_chart
+        }
+
+
+    def serialize_data(self):
+        return {
+            "id": self.id,
+            "name_chart": self.name_chart,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender
+        }
+
+
+#lista de starships favoritos
+class FavList_Starships(db.Model):
+     __tablename__ = 'favlist_starships'
+     id = db.Column(db.Integer, primary_key=True)
+     id_starship = db.Column(db.Integer,db.ForeignKey("starships_data.id"))
+     user_id = db.Column(db.Integer,db.ForeignKey("username_data.id"))
+     comments = db.Column(db.String(500), nullable=False)
+
+     def __repr__(self):
+         return f"<favlist_starships {self.user_id} - {self.id_starship}>"
+
+     def serialize(self):
+            return {
+                "id": self.id,
+                "user_id": self.user_id,
+                "name_starship": self.starships_data.name_starship,
+                "id_starship": self.id_starship,
+                "user_name": self.user.username,
+                "comments": self.comments
+     }
+
+     def serialize2(self):
+         return {
+             "id": self.starships_data.id,
+             "name_starship": self.starships_data.name_starship,
+             "model": self.starships_data.model,
+             "startship_class": self.starships_data.starship_class,
+             "manufacturer": self.starships_data.manufacturer,
+             "cost_in_credits": self.starships_data.cost_in_credits,
+             "length": self.starships_data.length,
+             "crew": self.starships_data.crew,
+             "passengers": self.starships_data.passengers,
+             "max_atmosphering_speed": self.starships_data.max_atmosphering_speed,
+             "hyperdrive_rating": self.starships_data.hyperdrive_rating,
+             "MGLT": self.starships_data.MGLT,
+             "cargo_capacity": self.starships_data.cargo_capacity,
+             "consumables": self.starships_data.consumables,
+             "pilots": self.starships_data.pilots
+         }
+
+#starships data
+class Starships_Data(db.Model):
+    __tablename__ = 'starships_data'
+    id = db.Column(db.Integer, primary_key=True)
+    name_starship = db.Column(db.String(100),nullable=False)
+    model = db.Column(db.String(250),unique=True, nullable=False)
+    starship_class = db.Column(db.String(250), nullable=False) 
+    manufacturer = db.Column(db.String(120), nullable=False)
+    cost_in_credits = db.Column(db.String(100) , nullable=False)
+    length = db.Column(db.String(100), nullable=False)
+    crew = db.Column(db.String(100), nullable=False)
+    passengers = db.Column(db.String(100), nullable=False)
+    max_atmosphering_speed = db.Column(db.String(100), nullable=False)
+    hyperdrive_rating = db.Column(db.String(100), nullable=False) 
+    MGLT = db.Column(db.String(100), nullable=False) 
+    cargo_capacity = db.Column(db.String(100), nullable=False) 
+    consumables = db.Column(db.String(25), nullable=False) 
+    pilots = db.Column(db.String(20), nullable=False)
+    fav_favlist_starships = db.relationship("FavList_Starships", backref="starships_data")
+
+    def __repr__(self):
+        return f"<starships_data {self.name_starship}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name_starship": self.name_starship,
+        }
+
+    def serialize_data(self):
+       return {
+           "id": self.id,
+           "name_starship": self.name_starship,
+           "model": self.model,
+           "starship_class": self.starship_class,
+           "manufacturer": self.manufacturer,
+           "cost_in_credits": self.cost_in_credits,
+           "length": self.length,
+           "crew": self.crew,
+           "passengers":self.passengers,
+           "max_atmosphering_speed": self.max_atmosphering_speed,
+           "hyperdrive_rating": self.hyperdrive_rating,
+           "MGLT": self.MGLT,
+           "cargo_capacity": self.cargo_capacity,
+           "consumables": self.consumables,
+           "pilots": self.pilots
+        }
+
+
+#- $ pipenv run migrate create database migrations (if models.py is edited)
+#- $ pipenv run upgrade run database migrations (if pending)
+#- $ pipenv run start start flask web server (if not running)
+#- $ pipenv run deploy deploy to heroku (if needed) 
